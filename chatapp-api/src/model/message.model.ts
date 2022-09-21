97a5a428266e54ce8b502da1d433aaddb34e3f57
iamsajith <sajithj03@gmail.com>
2022-09-21 17:39:28 +0530

    add message

--- chatapp-api/src/model/message.model.ts
+++ chatapp-api/src/model/message.model.ts
@@ -1,21 +1,28 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-
-
-
-
-const MessageSchema = new mongoose.Schema({
-  message: {
-    text: { type: String, required: true },
-  },
-  users: Array,
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-},
-{
-  timestamps: true,
+export interface MessageDocument extends mongoose.Document {
+  message: {text:string};
+  users: [string];
+  sender: mongoose.Types.ObjectId;
+  time: Date;
 }
-)
+
+const MessageSchema = new mongoose.Schema(
+  {
+    message: {
+      text: { type: String, required: true },
+    },
+    users: Array,
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model<MessageDocument>("messages", MessageSchema);
+export default Message;
--- chatapp-api/src/routes.ts
+++ chatapp-api/src/routes.ts
@@ -1,18 +1,21 @@
 import { Express, Request, Response } from "express";
 import { AuthController } from "./api/auth/controllers/auth.controllers";
 import { ChatController } from "./api/chats/controllers/chat.controller";
+import { MessageController } from "./api/message/controllers/message.controlls";
 import Container from "typedi";
 
 
 const routes = (app:Express)=>{
- const authController = Container.get(AuthController)
- const chatController = Container.get(ChatController)
+ const authController = Container.get(AuthController);
+ const chatController = Container.get(ChatController);
+ const messageController = Container.get(MessageController);
 
  app.post('/register',authController.SaveUserHandler)
  app.post('/login',authController.getUserHandler)
  app.put('/newpassword',authController.newPasswordHandler)
-
  app.get('/user/:id',chatController.getUserHandler)
+ app.post('/add-message',messageController.addMessageHandler)
+ app.post('/get-message',messageController.getMessageHandler)
 
 }
 
